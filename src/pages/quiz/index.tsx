0b2fa46bf5cc46c525dd7d8ddbd0c5b1ae34a256
youngduck <dudejr5839@naver.com>
2023-09-19 18:53:22 +0900

    퀴즈 틀

---
+++ src/pages/quiz/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const quiz = () => {
+  return (
+    <div className="w-full h-screen bg-dark-gray ">
+      <h1 className="bg-primary-blue">퀴즈zz</h1>
+      <div className="border-2 border-primary-blue rounded-xl m-2 w-[900px] h-[120px]">1</div>
+      <div className="border-2 border-primary-blue rounded-xl m-2 w-[900px] h-[120px]">2</div>
+      <div className="border-2 border-primary-blue rounded-xl m-2 w-[900px] h-[120px]">3</div>
+      <div className="border-2 border-primary-blue rounded-xl m-2 w-[900px] h-[120px]">4</div>
+    </div>
+  );
+};
+
+export default quiz;
