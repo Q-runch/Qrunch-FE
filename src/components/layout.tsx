226932661ef5529933253ffe2ce9146b692c4d75
youngduck <dudejr5839@naver.com>
2023-09-26 17:31:41 +0900

    fix: 레이아웃 삭제

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <>
       <Header />
-      <main className="mt-20 lg:w-2/3 lg:mx-auto xl:w-2/3 xl:mx-auto">{children}</main>
+      <main className="">{children}</main>
       <Footer />
     </>
   );
