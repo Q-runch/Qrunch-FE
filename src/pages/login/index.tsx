08865f8ecb96cacf8e34d59d9e0b19639fa93b28
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-10-17 17:05:56 +0900

    Feat : 로그인 구현 준비

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,10 @@
 const Login = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center sm:w-screen md:w-screen lg:w-screen">
-      <div className="text-primary-blue-solid">
-        <p>이렇게하면 /login으로 자동 라우팅됨. or pages/login.tsx</p>
+      <div className="w-1/3 h-3/5  sm:h-[250px] bg-white rounded-3xl  sm:border-0">
+        <div className="text-primary-blue-solid">
+          <p>이렇게하면 /login으로 자동 라우팅됨. or pages/login.tsx</p>
+        </div>
       </div>
     </div>
   );
