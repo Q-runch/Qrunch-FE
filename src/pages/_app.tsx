8a967be5b57b397ffdf60170a55d2b5154bdc4ce
youngduck <dudejr5839@naver.com>
2023-11-23 11:22:32 +0900

    fix: session provider폐기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,12 @@
 import Layout from '@/components/Layout/layout';
 import { wrapper } from '@/store/store';
 import '@/styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </SessionProvider>
+      <Component {...pageProps} />
     </Layout>
   );
 }
