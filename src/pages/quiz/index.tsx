0d3ee87664d8812ab6824879ab68eb6f35ef5cb9
youngduck <dudejr5839@naver.com>
2023-11-23 15:44:50 +0900

    feat: axios 설정

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import icons from '@/assets/icons/icon';
 import QuizItem from '@/components/quiz/QuizItem';
+import { axiosInstance } from '@/axiosInstance';
 
-const Quiz = () => {
+const Quiz = (props: any) => {
+  // console.log(props);
   return <QuizItem />;
+};
+
+export const getStaticProps = async () => {
+  try {
+    const response = await axiosInstance.get('/quiz/1');
+    const data = await response.data;
+    return {
+      props: {
+        quizDatas: data,
+      },
+    };
+  } catch (err) {
+    return {
+      props: {
+        error: 'error',
+      },
+    };
+  }
 };
 
 export default Quiz;
