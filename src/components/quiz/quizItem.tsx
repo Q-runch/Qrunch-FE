511b8ac31d0f9b29253ccd6412a3f647b75d787e
youngduck <dudejr5839@naver.com>
2023-11-25 16:41:55 +0900

    fetch

--- src/components/quiz/quizItem.tsx
+++ src/components/quiz/quizItem.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import { axiosInstance } from '@/axiosInstance';
 
 import { quizData } from '@/constant/quiz';
 
 const QuizItem = () => {
+  const abc = async () => {
+    const response = await axiosInstance.get('/summary');
+    const data = await response.data;
+    console.log('sdfsa', data);
+  };
+
+  const postQuiz = async () => {
+    // const response = await axiosInstance.post('/quiz/cd17464f-c823-4c10-94d7-cfa6edf3a49d');
+    const response = await fetch('https://sonkangjae.kro.kr/quiz/cd17464f-c823-4c10-94d7-cfa6edf3a49d');
+    const data = await response.json();
+    console.log('포스트', data);
+  };
+
   return (
     <div className="w-full h-screen flex items-center justify-center">
       {quizData.map((quiz, index) => (
         <div key={index} className="w-5/6 mx-auto">
           <h1 className="text-primary-blue-solid text-[30px] font-bold">{quiz.question}</h1>
+
           <ul className="list-none mt-6">
             {quiz.options.map((option, optionIndex) => (
               <li
@@ -22,11 +37,17 @@ const QuizItem = () => {
             ))}
           </ul>
           <div className="w-full flex justify-between mt-3">
-            <button className="w-[80px] h-[50px] border-2 border-primary-blue-solid rounded-xl text-primary-blue-solid hover:bg-primary-blue">
+            <button
+              onClick={postQuiz}
+              className="w-[80px] h-[50px] border-2 border-primary-blue-solid rounded-xl text-primary-blue-solid hover:bg-primary-blue"
+            >
               Prev
             </button>
-            <button className="w-[80px] h-[50px] border-2 border-primary-blue-solid rounded-xl text-primary-blue-solid hover:bg-primary-blue">
-              Next
+            <button
+              onClick={abc}
+              className="w-[80px] h-[50px] border-2 border-primary-blue-solid rounded-xl text-primary-blue-solid hover:bg-primary-blue"
+            >
+              Nextssf
             </button>
           </div>
         </div>
--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
-import icons from '@/assets/icons/icon';
 import QuizItem from '@/components/quiz/QuizItem';
 import { axiosInstance } from '@/axiosInstance';
 
-const Quiz = (props: any) => {
-  // console.log(props);
+const Quiz = () => {
   return <QuizItem />;
-};
-
-export const getStaticProps = async () => {
-  try {
-    const response = await axiosInstance.get('/quiz/1');
-    const data = await response.data;
-    return {
-      props: {
-        quizDatas: data,
-      },
-    };
-  } catch (err) {
-    return {
-      props: {
-        error: 'error',
-      },
-    };
-  }
 };
 
 export default Quiz;
