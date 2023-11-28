f6638c5bbecbd41378cee12e06c0c64ebb78d002
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-11-28 23:41:51 +0900

    Feat :  PDF Drag&Drop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,7 @@ const Home = function () {
   return (
     <div className="w-full h-screen flex items-center justify-center sm:w-screen md:w-screen lg:w-screen">
       <div className="w-2/3 h-4/5 text-primary-blue-solid  sm:h-[250px]  sm:border-0 d">
-        {activeButtonId === 1 && (
-          <div className="border-2 border-primary-blue-solid">
-            <UploadBox />
-          </div>
-        )}
+        {activeButtonId === 1 && <UploadBox />}
 
         {activeButtonId === 2 && <TextInput />}
       </div>
